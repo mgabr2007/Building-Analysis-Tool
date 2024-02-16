108b970fa598221b2400a3bc173268c163c7b53f
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2024-02-16 23:48:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -136,3 +136,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Using markdown to add copyright notice
+st.markdown("""
+---
+#### Copyright Notice
+Copyright (C) [2024] [Mostafa Gabr]. All rights reserved.
+
+This project is licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html).
+""")
